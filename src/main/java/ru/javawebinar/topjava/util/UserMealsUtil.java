152f92afbd6be09962def880ca5b62c1fc45248a
Marisha <yakovlevamava@gmail.com>
2016-11-20 02:03:54 +0300

    Prepare to HWO

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,13 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * GKislin
@@ -26,10 +28,45 @@ public class UserMealsUtil {
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
+
+/*  Test  */
+//        List<UserMealWithExceed> list = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+//        for (UserMealWithExceed e: list) {
+//            System.out.println(e.getDateTime() + " " + e.getDescription() + " " + e.getCalories() + " " + e.isExceed());
+//        }
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+
+
+/* Java 8 Stream API */
+        Map<LocalDate, Integer> m = mealList.stream()
+                .collect(Collectors.groupingBy((UserMeal meal) -> meal.getDateTime().toLocalDate(),
+                        Collectors.reducing(0, UserMeal::getCalories, Integer::sum)));
+
+        return mealList.stream()
+                .filter(e -> TimeUtil.isBetween(e.getDateTime().toLocalTime(), startTime, endTime))
+                .map(e -> {
+            LocalDate ld = e.getDateTime().toLocalDate();
+            return new UserMealWithExceed(e.getDateTime(), e.getDescription(), e.getCalories(), m.get(ld) > caloriesPerDay);
+        }).collect(Collectors.toList());
+
+/* Java 7 API */
+//      Map<LocalDate,Integer> mapOfCaliories = new HashMap<>();
+//        for (UserMeal meal: mealList) {
+//            LocalDate date = meal.getDateTime().toLocalDate();
+//            if (!mapOfCaliories.containsKey(date)) {
+//                mapOfCaliories.put(date, meal.getCalories());
+//            }
+//            else {
+//                mapOfCaliories.put(date, mapOfCaliories.get(date) + meal.getCalories());
+//            }
+//        }
+//        for (UserMeal meal: mealList) {
+//            if (TimeUtil.isBetween(meal.getDateTime().toLocalTime(),startTime,endTime)) {
+//                userMealWithExceedList.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(),mapOfCaliories.get(meal.getDateTime().toLocalDate())>caloriesPerDay));
+//            }
+//        }
+
     }
 }
